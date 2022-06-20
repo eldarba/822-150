ced5d18d7d6a3e9f8c671f16b8c748722205e770
eyal <narnavg@gmail.com>
2022-06-20 18:05:45 +0300

    arrays

---
+++ 20-06-22-basic-java/src/lab10/Tar02.java
@@ -0,0 +1,39 @@
+package lab10;
+
+import java.util.Arrays;
+
+public class Tar02 {
+
+	public static void main(String[] args) {
+		// create an array of random length
+		int length = (int) (Math.random() * 6) + 10;
+		int[] arr = new int[length];
+		// populate with random values
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 11);
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		// eliminate duplicates
+		int[] arrTemp = new int[arr.length];
+		int index = 0;
+		lbl: for (int i = 0; i < arr.length; i++) {
+			int candidate = arr[i];
+			for (int j = 0; j < index; j++) {
+				if(candidate == arrTemp[j]) {
+					continue lbl;
+				}
+			}
+			arrTemp[index++] = candidate;
+		}
+		
+		int[] arrShort = new int[index];
+		System.arraycopy(arrTemp, 0, arrShort, 0, index);
+//		System.out.println(Arrays.toString(arrTemp));
+//		System.out.println(Arrays.toString(arrShort));
+		arr = arrShort;
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
