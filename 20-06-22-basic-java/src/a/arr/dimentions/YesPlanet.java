aff3981195217773a8dd423c7205d8f5c16a1cf5
eyal <narnavg@gmail.com>
2022-06-20 20:05:13 +0300

    AAA

--- 20-06-22-basic-java/src/a/arr/dimentions/YesPlanet.java
+++ 20-06-22-basic-java/src/a/arr/dimentions/YesPlanet.java
@@ -3,9 +3,31 @@ package a.arr.dimentions;
 import java.util.Arrays;
 
 public class YesPlanet {
-	
+
+	// field
 	static char[][] mat = new char[5][10];
-	
+
+	// main - application bootstrap
+	public static void main(String[] args) {
+		// method invocation (call)
+		init();
+		print();
+		admit(1, 1);
+		admit(5, 10);
+		print();
+	}
+
+	// method definition
+	static void admit(int row, int seat) {
+		row--;
+		seat--;
+		if (mat[row][seat] == 'O') {
+			mat[row][seat] = 'X';
+		} else {
+			System.out.println("seat is taken");
+		}
+	}
+
 	static void init() {
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
@@ -13,11 +35,12 @@ public class YesPlanet {
 			}
 		}
 	}
-	
+
 	static void print() {
 		for (int i = 0; i < mat.length; i++) {
 			System.out.println(Arrays.toString(mat[i]));
 		}
+		System.out.println("========================");
 	}
 
 }
