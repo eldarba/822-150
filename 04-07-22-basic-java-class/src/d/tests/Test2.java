1319d95bc01c723e2b2d1a7ac446fb0d3013378a
zeev mindali <mindalizeev@gmail.com>
2022-07-04 21:48:07 +0300

    AAA

--- 04-07-22-basic-java-class/src/d/tests/Test2.java
+++ 04-07-22-basic-java-class/src/d/tests/Test2.java
@@ -6,9 +6,10 @@ import c.app.Car;
 public class Test2 {
 
 	public static void main(String[] args) {
-		
+
 		Car car = new Car();
 		PeterPan pan = new PeterPan();
 	}
 
 }
+
---
+++ 04-07-22-other-project/src/app/core/PeterPan.java
@@ -0,0 +1,5 @@
+package app.core;
+
+public class PeterPan {
+
+}
