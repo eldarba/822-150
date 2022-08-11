a60012c8509918cb16dbc3043262d0950f5a3193
eyal <narnavg@gmail.com>
2022-08-11 20:19:06 +0300

    AAA

--- 2022-08-11-enhancedSyntax/src/emps/EmpsUtil.java
+++ 2022-08-11-enhancedSyntax/src/emps/EmpsUtil.java
@@ -1,9 +1,24 @@
 package emps;
 
+import java.util.Collection;
+
 public class EmpsUtil {
 
-	public static double getAvgSalary() {
-		return null;
+	// this method will accept Collection<Employee>
+	// this method will accept Collection<Manager>
+	// this method will accept Collection<Direct>
+	// all have salary
+	public static double getAvgSalary(Collection<? extends Employee> emps) {
+		double sum = 0;
+		int c = 0;
+		for (Employee e : emps) {
+			if (e != null) {
+				sum += e.salary;
+				c++;
+			}
+		}
+		double avg = sum / c;
+		return avg;
 	}
 
 }
