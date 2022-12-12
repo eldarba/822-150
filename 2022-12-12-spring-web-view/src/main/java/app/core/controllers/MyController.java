15f196e3ffe8c089cc21c8e05628e7ebf3f59d8e
jbt <jbt@host.docker.internal>
2022-12-12 21:25:22 +0200

    AAA

--- 2022-12-12-spring-web-view/src/main/java/app/core/controllers/MyController.java
+++ 2022-12-12-spring-web-view/src/main/java/app/core/controllers/MyController.java
@@ -3,6 +3,7 @@ package app.core.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,11 @@ public class MyController {
 	@GetMapping("/greet")
 	public String greet() {
 		return "Hello";
+	}
+
+	@GetMapping("/add")
+	public double add(@RequestParam double a, @RequestParam double b) {
+		return a + b;
 	}
 
 }
