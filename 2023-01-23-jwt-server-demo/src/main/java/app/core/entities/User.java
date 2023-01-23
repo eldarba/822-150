3ff8ccbfd9cf37dcbb6a1f2de9a40cc2c4498d54
jbt <jbt@host.docker.internal>
2023-01-23 20:25:19 +0200

    AAA

--- 2023-01-23-jwt-server-demo/src/main/java/app/core/entities/User.java
+++ 2023-01-23-jwt-server-demo/src/main/java/app/core/entities/User.java
@@ -1,5 +1,12 @@
 package app.core.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,14 +14,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String email;
 	private String password;
 	private String firstName;
 	private String lastName;
 	private String username;
+	@Enumerated(EnumType.STRING)
 	private Role role;
 
 	public enum Role {
