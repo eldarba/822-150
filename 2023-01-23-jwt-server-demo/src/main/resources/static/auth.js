42a28dad42ca500b9bce7205cb76679acca7acbf
Eldar <eldarba@gmail.com>
2023-01-30 18:33:22 +0200

    JWT front

---
+++ 2023-01-23-jwt-server-demo/src/main/resources/static/auth.js
@@ -0,0 +1,9 @@
+function register(email, password, firstName, lastName, userName, role) {
+  console.log("register " + email);
+  console.log("register " + password);
+  console.log("register " + firstName);
+  console.log("register " + lastName);
+  console.log("register " + userName);
+  console.log("register " + role);
+  // create a User object here
+}
---
+++ 2023-01-23-jwt-server-demo/src/main/resources/static/models.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(id, email, password, firstName, lastName, userName, role) {
+        this.id = id;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.role = role;
+    }
+}
