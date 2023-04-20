44aea0f05d8a2cf82ff370839c156cc7231746a4
ranerlich7 <ranerlich@gmail.com>
2023-04-20 19:58:24 +0300

    AAA

--- module1/src/module-info.java
+++ module1/src/module-info.java
@@ -1,5 +1,9 @@
+// module declaration - set the module name
 module module1 {
 	
+	// module directives
+	// by default all packages are module private
+	// the exports directive sets which packages are public
 	exports module1.a;
 	exports module1.b;
 }
