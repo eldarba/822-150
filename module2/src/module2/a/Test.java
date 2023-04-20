f915ca5eb8a7a203ea15f2eb0998269d6a3a20e1
ranerlich7 <ranerlich@gmail.com>
2023-04-20 19:59:31 +0300

    AAA

---
+++ module2/src/module2/a/Test.java
@@ -0,0 +1,22 @@
+package module2.a;
+
+import module1.a.Hello;
+import module1.b.Car;
+// import module1.c.Util; // not accessible
+
+public class Test {
+
+	public static void main(String[] args) {
+		// using module2.a package
+		Hello hello = new Hello();
+		System.out.println(hello.greet("Dan"));
+		// using module2.b package
+		Car car = new Car();
+		car.drive(80);
+		// try using module2.c package
+		// Util u;
+		
+
+	}
+
+}
